42b8758e719986e9de6f187b6855529bc38cf418
neha <nehatyagi2516@gmail.com>
2020-04-09 18:40:08 +0530

    Successful Committed

---
+++ src/main/java/com/rtpl/common/Developer.java
@@ -0,0 +1,21 @@
+package com.rtpl.common;
+
+public class Developer {
+
+	private Language language;
+
+	
+
+	public Developer(Language language) {
+		this.language = language;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Developer [language=" + language + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/rtpl/common/Language.java
@@ -0,0 +1,20 @@
+package com.rtpl.common;
+
+public class Language {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+@Override
+public String toString() {
+	return "Language [name=" + name + "]";
+}
+
+	
+}
